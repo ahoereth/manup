5fe94eaf947953a48ec7283426d9568123a8f267
Alexander Höreth <a.hoereth@gmail.com>
2016-09-04 13:08:39 +0200

    Port original ManUp.js behavior to module style and refactor it.

---
+++ index.js
@@ -0,0 +1,45 @@
+const validTags = [
+  { tag: 'meta', name: 'mobile-web-app-capable',       manifestName: 'display', value: 'yes' },
+  { tag: 'meta', name: 'apple-mobile-web-app-capable', manifestName: 'display', value: 'yes' },
+  { tag: 'meta', name: 'apple-mobile-web-app-title', manifestName: 'short_name' },
+  { tag: 'meta', name: 'application-name',           manifestName: 'short_name' },
+  { tag: 'meta', name: 'msapplication-TileColor',    manifestName: 'ms_TileColor' },
+  { tag: 'meta', name: 'msapplication-square70x70logo',   manifestName: 'icons', imageSize: '70x70' },
+  { tag: 'meta', name: 'msapplication-square150x150logo', manifestName: 'icons', imageSize: '150x150' },
+  { tag: 'meta', name: 'msapplication-wide310x150logo',   manifestName: 'icons', imageSize: '310x150' },
+  { tag: 'meta', name: 'msapplication-square310x310logo', manifestName: 'icons', imageSize: '310x310' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '152x152' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '120x120' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '114x114' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '76x76' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '72x72' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '60x60' },
+  { tag: 'link', name: 'apple-touch-icon', manifestName: 'icons', imageSize: '57x57' },
+  { tag: 'link', name: 'icon', manifestName: 'icons', imageSize: '128x128' },
+  { tag: 'link', name: 'icon', manifestName: 'icons', imageSize: '192x192' },
+];
+
+
+module.exports = function manup(manifest) {
+  let tags = [];
+  validTags.forEach(({ tag, manifestName, name, imageSize, value }) => {
+    const target = manifest[manifestName];
+    if (target) {
+      console.log(target);
+      if (manifestName === 'icons') {
+        const icon = target.find(icon => icon.sizes === imageSize);
+        if (icon && icon.src) {
+          if (tag === 'link') {
+            tags.push(['link', { rel: name, sizes: imageSize, href: icon.src }]);
+          } else {
+            tags.push([tag, { name, content: icon.src }]);
+          }
+        }
+      } else {
+        const content = value ? value : target;
+        tags.push([tag, { name, content }]);
+      }
+    }
+  });
+  return tags;
+}
